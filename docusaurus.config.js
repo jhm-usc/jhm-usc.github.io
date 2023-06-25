6654e1c64de5671ba428400653e9290e2f8405b5
jhm-usc <jhmatth5@gmail.com>
2023-06-25 13:56:46 -0500

    fix deploy branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jhm-usc', // Usually your GitHub org/user name.
   projectName: 'jhm-usc.github.io', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
