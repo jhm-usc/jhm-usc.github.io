89f2d144db0d5dadf78d2ebe445c188365229541
jhm-usc <jhmatth5@gmail.com>
2023-06-25 20:38:05 -0500

    music and pics tweaks

--- src/components/Pictures.tsx
+++ src/components/Pictures.tsx
@@ -7,17 +7,18 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 export default function Pictures(): JSX.Element {
     return (
             <div className={styles.pictures}>
-                {PictureFileNames.map(picturePath => 
-                    <div>
-                        <h3>{ picturePath } A little somethin something</h3>
+                {PictureFileNames.map((picturePath, index) => 
+                    <div key={index}>
+                        <h3>{ index + 1 }: A little somethin something</h3>
                         <div>
-                            <LazyLoadImage src={'./showcase/' + picturePath}
-                            width='500px'
-                            height='auto'
-                            effect='blur'
-                            >
-                            </LazyLoadImage>
+                            <LazyLoadImage 
+                                src={'./showcase/' + picturePath}
+                                width='500px'
+                                height='auto'
+                                effect='blur'
+                            />
                         </div>
+                        <p>Caption or something maybe add here</p>
                         <hr></hr>
                         <br></br>
                         <br></br>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,13 @@ export default function Home(): JSX.Element {
     return (
       <Layout title={`Some neat stuff`}>
         <main>
+            <iframe 
+              width="0"
+              height="0" 
+              src="https://www.youtube.com/embed/25BkVBgFD9Y?autoplay=1&mute=0" 
+              title="YouTube video player" 
+              allow="autoplay; encrypted-media; gyroscope;" 
+              ></iframe>
             <Pictures />
         </main>
       </Layout>
